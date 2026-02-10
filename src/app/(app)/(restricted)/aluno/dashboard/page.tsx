cf113786bff3cac8989011183dcd31d5df47f29f
Amanda Padilha <amandaportopadilha@gmail.com>
2026-02-10 20:38:47 +0000

    better, but charts are still clipping when i reduce window height

--- src/app/(app)/(restricted)/aluno/dashboard/page.tsx
+++ src/app/(app)/(restricted)/aluno/dashboard/page.tsx
@@ -3,9 +3,11 @@ import { TestsBarChart } from "@/components/tests-bar-chart";
 
 export default function Page() {
   return (
-    <div className="flex h-full w-full flex-col gap-8 p-4 sm:p-10">
-      <TestsBarChart />
-      <GcpLineChart />
+    <div className="h-full w-full p-4 sm:p-10">
+      <div className="flex h-full w-full flex-col gap-8">
+        <TestsBarChart />
+        <GcpLineChart />
+      </div>
     </div>
   );
 }
