21a0d36bc1ad6ec36479329acf658baa07746257
Pugialli <joaopugialli@gmail.com>
2026-02-10 14:12:24 -0300

    prisma adjust

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -87,7 +87,7 @@ function SelectContent({
         data-slot="select-content"
         className={cn(
           "bg-popover text-popover-foreground data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 data-closed:zoom-out-95 data-open:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 ring-foreground/10 rounded-lg border border-input shadow-lg ring-1 duration-100 relative z-50 overflow-hidden",
-          "w-[var(--radix-select-trigger-width)] max-h-[300px]",
+          "w-(--radix-select-trigger-width) max-h-75",
           className
         )}
         position={position}
