553c4f46bf2e83da823da9b5d694d1c4159ba053
Amanda Padilha <amandaportopadilha@gmail.com>
2026-02-12 12:51:58 +0000

    please reduce the gap between charts by half

--- src/app/(app)/(restricted)/aluno/dashboard/page.tsx
+++ src/app/(app)/(restricted)/aluno/dashboard/page.tsx
@@ -5,11 +5,11 @@ import { TestsBarChart } from "@/components/tests-bar-chart";
 export default function Page() {
   return (
     <div className="h-[calc(100vh-theme(space.24))] w-full p-4 sm:p-10">
-      <div className="grid h-full w-full grid-rows-2 gap-8">
+      <div className="grid h-full w-full grid-rows-2 gap-4">
         <div className="min-h-0">
           <TestsBarChart />
         </div>
-        <div className="grid min-h-0 grid-cols-2 gap-8">
+        <div className="grid min-h-0 grid-cols-2 gap-4">
           <GcpLineChart />
           <HabilidadesChart />
         </div>
